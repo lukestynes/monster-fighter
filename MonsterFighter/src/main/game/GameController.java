0a2dd2282529fb5976c5b710c9609f4674ce6a18
Luke Stynes <stynesluke@gmail.com>
2022-05-24 10:15:53 +1200

    Create setupScreen method to take initial user input

--- MonsterFighter/src/main/game/GameController.java
+++ MonsterFighter/src/main/game/GameController.java
@@ -1,6 +1,10 @@
 package main.game;
 
+import main.ui.*;
+
 public class GameController {
+	
+	CLInterface cli = new CLInterface();
 
 	public static void main(String[] args) {
 		GameController game = new GameController();
@@ -8,10 +12,17 @@ public class GameController {
 	}
 	
 	public void run() {
+		//Create an interface instance
 		
+		gameSetup();
 	}
 	
 	public void gameSetup() {
-		
+		cli.setupScreen();
+	}
+		//TODO: Get player name 3-15 chars
+		//TODO: Get game length 5-15 days
+		//TODO: Choose a starting monster for your team
+		//TODO: Choose a difficulty
 	}
 
--- MonsterFighter/src/main/ui/CLInterface.java
+++ MonsterFighter/src/main/ui/CLInterface.java
@@ -1,5 +1,32 @@
 package main.ui;
 
-public class CLInterface {
+import java.util.Scanner;
 
+public class CLInterface {
+	
+	Scanner scan = new Scanner(System.in);
+	
+	public void setupScreen() {
+		String name;
+		int length;
+		int difficulty;
+		//TODO: Starting monster
+		
+		
+		System.out.println("###############################################################");
+		System.out.println("Welcome to Monster Fighter");
+		System.out.println("Made by Luke\n\n");
+		
+		System.out.println("What is your name?");
+		name = scan.nextLine();
+		
+		System.out.println("How long do you want the game to be? (5-15 days)");
+		length = scan.nextInt();
+		
+		System.out.println("What difficulty do you want to play on? (0=easy, 1=hard)");
+		difficulty = scan.nextInt();
+		
+		System.out.println("Pause");
+		
+	}
 }
