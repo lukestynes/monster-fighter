b8533432b7886e48ab168e2265def1ca666c74c7
Luke Stynes <stynesluke@gmail.com>
2022-05-24 12:23:04 +1200

    Create all the individual monster types and set level values

--- MonsterFighter/src/main/monsters/EarthMonster.java
+++ MonsterFighter/src/main/monsters/EarthMonster.java
@@ -1,15 +1,14 @@
 package main.monsters;
 
-
-public class FireMonster extends Monster {
-	//All of the values for the monster at levels 1, 2, 3, and 4
-	private final int[] MAX_HEALTH = {100, 125, 150, 175};
-	private final int[] HEAL_AMOUNT = {20, 30, 40, 50};
-	private final int[] DAMAGE = {25, 30, 35, 40};
-	private final int[] DEFENCE = {10, 15, 20, 25};
+public class EarthMonster extends Monster {
+//All of the values for the monster at levels 1, 2, 3, and 4
+	private final int[] MAX_HEALTH = {100, 120, 175, 200};
+	private final int[] HEAL_AMOUNT = {10, 20, 40, 50};
+	private final int[] DAMAGE = {10, 15, 35, 40};
+	private final int[] DEFENCE = {5, 10, 20, 25};
 	
 	//Default level 1 monster
-	public FireMonster() {
+	public EarthMonster() {
 		this.setMaxHealth(MAX_HEALTH[0]);
 		this.setCurrentHealth(getMaxHealth());
 		
@@ -17,11 +16,11 @@ public class FireMonster extends Monster {
 		this.setDamage(DAMAGE[0]);
 		this.setDefence(DEFENCE[0]);
 		
-		this.setName("Fire Monster LVL 1");
+		this.setName("Earth Monster LVL 1");
 	}
 	
 	//Constructs a monster at a specified level
-	public FireMonster(int level) {
+	public EarthMonster(int level) {
 		int index = level--;
 		
 		this.setMaxHealth(MAX_HEALTH[index]);
@@ -31,6 +30,7 @@ public class FireMonster extends Monster {
 		this.setDamage(DAMAGE[index]);
 		this.setDefence(DEFENCE[index]);
 		
-		this.setName("Fire Monster LVL " + level);
+		//TODO: REMOVE THIS AND ADD NAME GENERATOR
+		this.setName("Earth Monster LVL " + level);
 	}
 }
--- MonsterFighter/src/main/monsters/ElectricityMonster.java
+++ MonsterFighter/src/main/monsters/ElectricityMonster.java
@@ -1,15 +1,14 @@
 package main.monsters;
 
-
-public class FireMonster extends Monster {
-	//All of the values for the monster at levels 1, 2, 3, and 4
+public class ElectricityMonster extends Monster {
+//All of the values for the monster at levels 1, 2, 3, and 4
 	private final int[] MAX_HEALTH = {100, 125, 150, 175};
 	private final int[] HEAL_AMOUNT = {20, 30, 40, 50};
-	private final int[] DAMAGE = {25, 30, 35, 40};
-	private final int[] DEFENCE = {10, 15, 20, 25};
+	private final int[] DAMAGE = {30, 35, 40, 45};
+	private final int[] DEFENCE = {5, 10, 15, 20};
 	
 	//Default level 1 monster
-	public FireMonster() {
+	public ElectricityMonster() {
 		this.setMaxHealth(MAX_HEALTH[0]);
 		this.setCurrentHealth(getMaxHealth());
 		
@@ -17,11 +16,11 @@ public class FireMonster extends Monster {
 		this.setDamage(DAMAGE[0]);
 		this.setDefence(DEFENCE[0]);
 		
-		this.setName("Fire Monster LVL 1");
+		this.setName("Electricity Monster LVL 1");
 	}
 	
 	//Constructs a monster at a specified level
-	public FireMonster(int level) {
+	public ElectricityMonster(int level) {
 		int index = level--;
 		
 		this.setMaxHealth(MAX_HEALTH[index]);
@@ -31,6 +30,7 @@ public class FireMonster extends Monster {
 		this.setDamage(DAMAGE[index]);
 		this.setDefence(DEFENCE[index]);
 		
-		this.setName("Fire Monster LVL " + level);
+		//TODO: REMOVE THIS AND ADD NAME GENERATOR
+		this.setName("Electricity Monster LVL " + level);
 	}
 }
