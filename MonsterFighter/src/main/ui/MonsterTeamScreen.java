445292ad7280e34b48f2b9beaed0d074d26b8ac8
Luke Stynes <stynesluke@gmail.com>
2022-05-25 19:48:04 +1200

    Clean up monster teams code

--- MonsterFighter/src/main/ui/MonsterTeamScreen.java
+++ MonsterFighter/src/main/ui/MonsterTeamScreen.java
@@ -16,6 +16,7 @@ import javax.swing.JTextPane;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JCheckBox;
 
 public class MonsterTeamScreen {
 
@@ -48,34 +49,6 @@ public class MonsterTeamScreen {
 		frame.setLocationRelativeTo(null);
 		frame.getContentPane().setLayout(null);
 		
-		JLabel lblPlayerName = new JLabel(gui.getGame().getPlayer().getName());
-		lblPlayerName.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
-		lblPlayerName.setBounds(6, 4, 117, 31);
-		frame.getContentPane().add(lblPlayerName);
-		
-		JLabel lblScore = new JLabel("Score: " + gui.getGame().getPlayer().getScore());
-		lblScore.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
-		lblScore.setBounds(6, 32, 117, 31);
-		frame.getContentPane().add(lblScore);
-		
-		JLabel lblMonsterTeam = new JLabel("Monster Team");
-		lblMonsterTeam.setHorizontalAlignment(SwingConstants.CENTER);
-		lblMonsterTeam.setFont(new Font("Lucida Grande", Font.BOLD, 26));
-		lblMonsterTeam.setBounds(0, 0, 1000, 63);
-		frame.getContentPane().add(lblMonsterTeam);
-		
-		JLabel lblDayCount = new JLabel("Day: " + gui.getGame().getCurrentDay() + "/" + gui.getGame().getGameLength());
-		lblDayCount.setHorizontalAlignment(SwingConstants.TRAILING);
-		lblDayCount.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
-		lblDayCount.setBounds(877, 4, 117, 31);
-		frame.getContentPane().add(lblDayCount);
-		
-		JLabel lblGoldCount = new JLabel("Gold: " + gui.getGame().getPlayer().getGold());
-		lblGoldCount.setHorizontalAlignment(SwingConstants.TRAILING);
-		lblGoldCount.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
-		lblGoldCount.setBounds(877, 32, 117, 31);
-		frame.getContentPane().add(lblGoldCount);
-		
 		JPanel panelMonster1 = new JPanel();
 		panelMonster1.setBounds(34, 231, 200, 250);
 		frame.getContentPane().add(panelMonster1);
@@ -85,34 +58,9 @@ public class MonsterTeamScreen {
 		txtpMonst1.setBounds(0, 0, 200, 250);
 		panelMonster1.add(txtpMonst1);
 		
-		ArrayList<Monster> monsterTeam = gui.getGame().getPlayer().getMonsterTeam().getMonsterTeamList();
-		String monst1 = "This Slot is Empty";
-		String monst2, monst3, monst4;
-		monst4 = monst2 = monst3 = monst1;
+		ArrayList<String> monsterStrings = gui.updateMonsters();
 		
-		
-		switch (monsterTeam.size()) {
-		case 1:
-			monst1 = String.format("%s\nType: %s \nLevel: %s\nDescription: %s\n\nHealth: %d\nHeal Amount: %d\nDefence: %d\nDamage: %d\n", monsterTeam.get(0).getName(), monsterTeam.get(0).getType(), monsterTeam.get(0).getLevel(), monsterTeam.get(0).getDescription(), monsterTeam.get(0).getMaxHealth(), monsterTeam.get(0).getHealAmount(), monsterTeam.get(0).getDefence(), monsterTeam.get(0).getDamage());
-			break;
-		case 2:
-			monst1 = String.format("%s\nType: %s \nLevel: %s\nDescription: %s\n\nHealth: %d\nHeal Amount: %d\nDefence: %d\nDamage: %d\n", monsterTeam.get(0).getName(), monsterTeam.get(0).getType(),monsterTeam.get(0).getLevel(), monsterTeam.get(0).getDescription(), monsterTeam.get(0).getMaxHealth(), monsterTeam.get(0).getHealAmount(), monsterTeam.get(0).getDefence(), monsterTeam.get(0).getDamage());
-			monst2 = String.format("%s\nType: %s \nLevel: %s\nDescription: %s\n\nHealth: %d\nHeal Amount: %d\nDefence: %d\nDamage: %d\n", monsterTeam.get(1).getName(), monsterTeam.get(0).getType(),monsterTeam.get(1).getLevel(), monsterTeam.get(1).getDescription(), monsterTeam.get(1).getMaxHealth(), monsterTeam.get(1).getHealAmount(), monsterTeam.get(1).getDefence(), monsterTeam.get(1).getDamage());
-			break;
-		case 3:
-			monst1 = String.format("%s\nType: %s \nLevel: %s\nDescription: %s\n\nHealth: %d\nHeal Amount: %d\nDefence: %d\nDamage: %d\n", monsterTeam.get(0).getName(), monsterTeam.get(0).getType(),monsterTeam.get(0).getLevel(), monsterTeam.get(0).getDescription(), monsterTeam.get(0).getMaxHealth(), monsterTeam.get(0).getHealAmount(), monsterTeam.get(0).getDefence(), monsterTeam.get(0).getDamage());
-			monst2 = String.format("%s\nType: %s \nLevel: %s\nDescription: %s\n\nHealth: %d\nHeal Amount: %d\nDefence: %d\nDamage: %d\n", monsterTeam.get(1).getName(), monsterTeam.get(0).getType(),monsterTeam.get(1).getLevel(), monsterTeam.get(1).getDescription(), monsterTeam.get(1).getMaxHealth(), monsterTeam.get(1).getHealAmount(), monsterTeam.get(1).getDefence(), monsterTeam.get(1).getDamage());
-			monst3 = String.format("%s\nType: %s \nLevel: %s\nDescription: %s\n\nHealth: %d\nHeal Amount: %d\nDefence: %d\nDamage: %d\n", monsterTeam.get(2).getName(), monsterTeam.get(0).getType(),monsterTeam.get(2).getLevel(), monsterTeam.get(2).getDescription(), monsterTeam.get(2).getMaxHealth(), monsterTeam.get(2).getHealAmount(), monsterTeam.get(2).getDefence(), monsterTeam.get(2).getDamage());
-			break;
-		case 4:
-			monst1 = String.format("%s\nType: %s \nLevel: %s\nDescription: %s\n\nHealth: %d\nHeal Amount: %d\nDefence: %d\nDamage: %d\n", monsterTeam.get(0).getName(), monsterTeam.get(0).getType(),monsterTeam.get(0).getLevel(), monsterTeam.get(0).getDescription(), monsterTeam.get(0).getMaxHealth(), monsterTeam.get(0).getHealAmount(), monsterTeam.get(0).getDefence(), monsterTeam.get(0).getDamage());
-			monst2 = String.format("%s\nType: %s \nLevel: %s\nDescription: %s\n\nHealth: %d\nHeal Amount: %d\nDefence: %d\nDamage: %d\n", monsterTeam.get(1).getName(), monsterTeam.get(0).getType(),monsterTeam.get(1).getLevel(), monsterTeam.get(1).getDescription(), monsterTeam.get(1).getMaxHealth(), monsterTeam.get(1).getHealAmount(), monsterTeam.get(1).getDefence(), monsterTeam.get(1).getDamage());
-			monst3 = String.format("%s\nType: %s \nLevel: %s\nDescription: %s\n\nHealth: %d\nHeal Amount: %d\nDefence: %d\nDamage: %d\n", monsterTeam.get(2).getName(), monsterTeam.get(0).getType(),monsterTeam.get(2).getLevel(), monsterTeam.get(2).getDescription(), monsterTeam.get(2).getMaxHealth(), monsterTeam.get(2).getHealAmount(), monsterTeam.get(2).getDefence(), monsterTeam.get(2).getDamage());
-			monst4 = String.format("%s\nType: %s \nLevel: %s\nDescription: %s\n\nHealth: %d\nHeal Amount: %d\nDefence: %d\nDamage: %d\n", monsterTeam.get(3).getName(), monsterTeam.get(0).getType(),monsterTeam.get(3).getLevel(), monsterTeam.get(3).getDescription(), monsterTeam.get(3).getMaxHealth(), monsterTeam.get(3).getHealAmount(), monsterTeam.get(3).getDefence(), monsterTeam.get(3).getDamage());
-			break;
-		
-		}
-		txtpMonst1.setText(monst1);
+		txtpMonst1.setText(monsterStrings.get(0));
 		txtpMonst1.setEditable(false);
 		
 		JPanel panelMonster4 = new JPanel();
@@ -121,7 +69,7 @@ public class MonsterTeamScreen {
 		panelMonster4.setLayout(null);
 		
 		JTextPane txtpMonst1_3 = new JTextPane();
-		txtpMonst1_3.setText(monst2);
+		txtpMonst1_3.setText(monsterStrings.get(1));
 		txtpMonst1_3.setEditable(false);
 		txtpMonst1_3.setBounds(0, 0, 200, 250);
 		panelMonster4.add(txtpMonst1_3);
@@ -132,7 +80,7 @@ public class MonsterTeamScreen {
 		panelMonster2.setLayout(null);
 		
 		JTextPane txtpMonst1_1 = new JTextPane();
-		txtpMonst1_1.setText(monst3);
+		txtpMonst1_1.setText(monsterStrings.get(2));
 		txtpMonst1_1.setEditable(false);
 		txtpMonst1_1.setBounds(0, 0, 200, 250);
 		panelMonster2.add(txtpMonst1_1);
@@ -143,7 +91,7 @@ public class MonsterTeamScreen {
 		panelMonster3.setLayout(null);
 		
 		JTextPane txtpMonst1_2 = new JTextPane();
-		txtpMonst1_2.setText(monst4);
+		txtpMonst1_2.setText(monsterStrings.get(3));
 		txtpMonst1_2.setEditable(false);
 		txtpMonst1_2.setBounds(0, 0, 200, 250);
 		panelMonster3.add(txtpMonst1_2);
@@ -155,8 +103,103 @@ public class MonsterTeamScreen {
 				gui.closeMonsterTeamScreen(gui.getMonsterScreen());
 			}
 		});
-		btnReturn.setBounds(423, 549, 161, 29);
+		btnReturn.setBounds(424, 608, 161, 29);
 		frame.getContentPane().add(btnReturn);
+		
+		JPanel panelTopBar = new JPanel();
+		panelTopBar.setBounds(0, 4, 1000, 71);
+		frame.getContentPane().add(panelTopBar);
+		panelTopBar.setLayout(null);
+		
+		JLabel lblPlayerName = new JLabel(gui.getGame().getPlayer().getName());
+		lblPlayerName.setBounds(1, -3, 117, 31);
+		panelTopBar.add(lblPlayerName);
+		lblPlayerName.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+		
+		JLabel lblScore = new JLabel("Score: " + gui.getGame().getPlayer().getScore());
+		lblScore.setBounds(1, 30, 117, 31);
+		panelTopBar.add(lblScore);
+		lblScore.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
+		
+		JLabel lblDayCount = new JLabel("Day: " + gui.getGame().getCurrentDay() + "/" + gui.getGame().getGameLength());
+		lblDayCount.setBounds(881, 1, 117, 31);
+		panelTopBar.add(lblDayCount);
+		lblDayCount.setHorizontalAlignment(SwingConstants.TRAILING);
+		lblDayCount.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+		
+		JLabel lblGoldCount = new JLabel("Gold: " + gui.getGame().getPlayer().getGold());
+		lblGoldCount.setBounds(881, 37, 117, 31);
+		panelTopBar.add(lblGoldCount);
+		lblGoldCount.setHorizontalAlignment(SwingConstants.TRAILING);
+		lblGoldCount.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+		
+		JLabel lblMonsterTeam = new JLabel("Monster Team");
+		lblMonsterTeam.setBounds(-2, 3, 1000, 63);
+		panelTopBar.add(lblMonsterTeam);
+		lblMonsterTeam.setHorizontalAlignment(SwingConstants.CENTER);
+		lblMonsterTeam.setFont(new Font("Lucida Grande", Font.BOLD, 26));
+		
+		JCheckBox chckbx1 = new JCheckBox("");
+		chckbx1.setHorizontalAlignment(SwingConstants.CENTER);
+		chckbx1.setBounds(34, 481, 200, 23);
+		frame.getContentPane().add(chckbx1);
+		
+		
+		JCheckBox chckbx2 = new JCheckBox("");
+		chckbx2.setHorizontalAlignment(SwingConstants.CENTER);
+		chckbx2.setBounds(285, 481, 200, 23);
+		frame.getContentPane().add(chckbx2);
+		
+	
+		JCheckBox chckbx3 = new JCheckBox("");
+		chckbx3.setHorizontalAlignment(SwingConstants.CENTER);
+		chckbx3.setBounds(525, 481, 200, 23);
+		frame.getContentPane().add(chckbx3);
+	
+		
+		JCheckBox chckbx4 = new JCheckBox("");
+		chckbx4.setHorizontalAlignment(SwingConstants.CENTER);
+		chckbx4.setBounds(769, 481, 200, 23);
+		frame.getContentPane().add(chckbx4);
+	
+		ArrayList<JCheckBox> checkedMonsters = new ArrayList<>(4);
+		ArrayList<Monster> monsterTeam = gui.getGame().getPlayer().getMonsterTeam().getMonsterTeamList();
+		
+		checkedMonsters.add(chckbx1);
+		checkedMonsters.add(chckbx2);
+		checkedMonsters.add(chckbx3);
+		checkedMonsters.add(chckbx4);
+		
+		int count = 0;
+		for (JCheckBox checkbox: checkedMonsters) {
+			if (count < monsterTeam.size()) {
+				checkbox.setEnabled(true);
+			} else {
+				checkbox.setEnabled(false);
+			}
+			
+			count++;
+		}
+		
+		
+		JButton btnSellSelectedMonsters = new JButton("Sell Selected Monsters");
+		btnSellSelectedMonsters.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				for (JCheckBox checkbox: checkedMonsters) {
+					
+					int count = 0;
+					if (checkbox.isSelected()) {
+						gui.getGame().getShop().returnMonster(gui.getGame().getPlayer().getMonsterTeam().getMonsterTeamList().get(count), gui.getGame().getPlayer());
+						count++;
+					}
+				}
+				
+				gui.closeMonsterTeamScreen(gui.getMonsterScreen());
+				gui.launchMonsterTeamScreen();
+			}
+		});
+		btnSellSelectedMonsters.setBounds(392, 545, 205, 29);
+		frame.getContentPane().add(btnSellSelectedMonsters);
 	}
 
 }
