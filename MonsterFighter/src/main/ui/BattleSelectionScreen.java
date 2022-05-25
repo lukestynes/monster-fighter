eec9204c368907d0c37a20490f5f2f6fb7e9d235
Luke Stynes <stynesluke@gmail.com>
2022-05-25 16:46:01 +1200

    Add future screen for battles

---
+++ MonsterFighter/src/main/ui/BattleSelectionScreen.java
@@ -0,0 +1,38 @@
+package main.ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class BattleSelectionScreen {
+
+	private JFrame frame;
+	private GUIController gui;
+
+	public BattleSelectionScreen(GUIController gui) {
+		this.gui = gui;
+		initialize();
+		frame.setVisible(true);
+	}
+	
+	public void closeWindow() {
+		frame.dispose();
+	}
+	
+	public void finishedWindow() {
+		gui.closeBattleSelectionScreen(this);
+	}
+	
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//Centers the window
+		frame.setLocationRelativeTo(null);
+	}
+
+}
