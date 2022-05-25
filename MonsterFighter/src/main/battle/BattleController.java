674bdec624653c424e2fdc34bb344ee143529d51
Luke Stynes <stynesluke@gmail.com>
2022-05-25 23:43:43 +1200

    Add BattleController to handle the generation and management of battles

--- MonsterFighter/src/main/battle/BattleController.java
+++ MonsterFighter/src/main/battle/BattleController.java
@@ -1,5 +1,13 @@
 package main.battle;
 
-public class BattleController {
+import main.game.GameController;
 
+public class BattleController {
+	private GameController game;
+	
+	private ArrayList<Battle>
+	
+	public BattleController(GameController game) {
+		this.game = game;
+	}
 }
