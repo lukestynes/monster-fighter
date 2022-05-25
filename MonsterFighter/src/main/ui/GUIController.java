9fdd67d574d1f26863903bfb222c0c535d430f71
Luke Stynes <stynesluke@gmail.com>
2022-05-25 19:47:33 +1200

    Add methods for updating monster tiles

--- MonsterFighter/src/main/ui/GUIController.java
+++ MonsterFighter/src/main/ui/GUIController.java
@@ -23,6 +23,45 @@ public class GUIController {
 	public GameController getGame() {
 		return this.game;
 	}
+	
+	public ArrayList<String> updateMonsters() {
+		ArrayList<Monster> monsterTeam = game.getPlayer().getMonsterTeam().getMonsterTeamList();
+		String monst1 = "This Slot is Empty";
+		String monst2, monst3, monst4;
+		monst4 = monst2 = monst3 = monst1;
+		
+		
+		switch (monsterTeam.size()) {
+		case 1:
+			monst1 = String.format("%s\nType: %s \nLevel: %s\nDescription: %s\n\nHealth: %d\nHeal Amount: %d\nDefence: %d\nDamage: %d\n", monsterTeam.get(0).getName(), monsterTeam.get(0).getType(), monsterTeam.get(0).getLevel(), monsterTeam.get(0).getDescription(), monsterTeam.get(0).getMaxHealth(), monsterTeam.get(0).getHealAmount(), monsterTeam.get(0).getDefence(), monsterTeam.get(0).getDamage());
+			break;
+		case 2:
+			monst1 = String.format("%s\nType: %s \nLevel: %s\nDescription: %s\n\nHealth: %d\nHeal Amount: %d\nDefence: %d\nDamage: %d\n", monsterTeam.get(0).getName(), monsterTeam.get(0).getType(),monsterTeam.get(0).getLevel(), monsterTeam.get(0).getDescription(), monsterTeam.get(0).getMaxHealth(), monsterTeam.get(0).getHealAmount(), monsterTeam.get(0).getDefence(), monsterTeam.get(0).getDamage());
+			monst2 = String.format("%s\nType: %s \nLevel: %s\nDescription: %s\n\nHealth: %d\nHeal Amount: %d\nDefence: %d\nDamage: %d\n", monsterTeam.get(1).getName(), monsterTeam.get(0).getType(),monsterTeam.get(1).getLevel(), monsterTeam.get(1).getDescription(), monsterTeam.get(1).getMaxHealth(), monsterTeam.get(1).getHealAmount(), monsterTeam.get(1).getDefence(), monsterTeam.get(1).getDamage());
+			break;
+		case 3:
+			monst1 = String.format("%s\nType: %s \nLevel: %s\nDescription: %s\n\nHealth: %d\nHeal Amount: %d\nDefence: %d\nDamage: %d\n", monsterTeam.get(0).getName(), monsterTeam.get(0).getType(),monsterTeam.get(0).getLevel(), monsterTeam.get(0).getDescription(), monsterTeam.get(0).getMaxHealth(), monsterTeam.get(0).getHealAmount(), monsterTeam.get(0).getDefence(), monsterTeam.get(0).getDamage());
+			monst2 = String.format("%s\nType: %s \nLevel: %s\nDescription: %s\n\nHealth: %d\nHeal Amount: %d\nDefence: %d\nDamage: %d\n", monsterTeam.get(1).getName(), monsterTeam.get(0).getType(),monsterTeam.get(1).getLevel(), monsterTeam.get(1).getDescription(), monsterTeam.get(1).getMaxHealth(), monsterTeam.get(1).getHealAmount(), monsterTeam.get(1).getDefence(), monsterTeam.get(1).getDamage());
+			monst3 = String.format("%s\nType: %s \nLevel: %s\nDescription: %s\n\nHealth: %d\nHeal Amount: %d\nDefence: %d\nDamage: %d\n", monsterTeam.get(2).getName(), monsterTeam.get(0).getType(),monsterTeam.get(2).getLevel(), monsterTeam.get(2).getDescription(), monsterTeam.get(2).getMaxHealth(), monsterTeam.get(2).getHealAmount(), monsterTeam.get(2).getDefence(), monsterTeam.get(2).getDamage());
+			break;
+		case 4:
+			monst1 = String.format("%s\nType: %s \nLevel: %s\nDescription: %s\n\nHealth: %d\nHeal Amount: %d\nDefence: %d\nDamage: %d\n", monsterTeam.get(0).getName(), monsterTeam.get(0).getType(),monsterTeam.get(0).getLevel(), monsterTeam.get(0).getDescription(), monsterTeam.get(0).getMaxHealth(), monsterTeam.get(0).getHealAmount(), monsterTeam.get(0).getDefence(), monsterTeam.get(0).getDamage());
+			monst2 = String.format("%s\nType: %s \nLevel: %s\nDescription: %s\n\nHealth: %d\nHeal Amount: %d\nDefence: %d\nDamage: %d\n", monsterTeam.get(1).getName(), monsterTeam.get(0).getType(),monsterTeam.get(1).getLevel(), monsterTeam.get(1).getDescription(), monsterTeam.get(1).getMaxHealth(), monsterTeam.get(1).getHealAmount(), monsterTeam.get(1).getDefence(), monsterTeam.get(1).getDamage());
+			monst3 = String.format("%s\nType: %s \nLevel: %s\nDescription: %s\n\nHealth: %d\nHeal Amount: %d\nDefence: %d\nDamage: %d\n", monsterTeam.get(2).getName(), monsterTeam.get(0).getType(),monsterTeam.get(2).getLevel(), monsterTeam.get(2).getDescription(), monsterTeam.get(2).getMaxHealth(), monsterTeam.get(2).getHealAmount(), monsterTeam.get(2).getDefence(), monsterTeam.get(2).getDamage());
+			monst4 = String.format("%s\nType: %s \nLevel: %s\nDescription: %s\n\nHealth: %d\nHeal Amount: %d\nDefence: %d\nDamage: %d\n", monsterTeam.get(3).getName(), monsterTeam.get(0).getType(),monsterTeam.get(3).getLevel(), monsterTeam.get(3).getDescription(), monsterTeam.get(3).getMaxHealth(), monsterTeam.get(3).getHealAmount(), monsterTeam.get(3).getDefence(), monsterTeam.get(3).getDamage());
+			break;
+		}
+		
+		ArrayList<String> words = new ArrayList<String>(4);
+		words.add(monst1);
+		words.add(monst2);
+		words.add(monst3);
+		words.add(monst4);
+		
+		return words;
+	}
+	
+	
 
 	public void launchSetupScreen() {
 		System.out.println("DEBUG [GUICONTROLLER]: LAUNCHING SETUP SCREEN");
@@ -74,6 +113,18 @@ public class GUIController {
 	
 	public InventoryScreen getInventoryScreen() {
 		return this.inventoryWindow;
+	}
+	
+	public void launchShopScreen() {
+		shopWindow = new ShopScreen(this);
+	}
+	
+	public void closeShopScreen(ShopScreen shopWindow) {
+		shopWindow.closeWindow();
+	}
+	
+	public ShopScreen getShopScreen() {
+		return this.shopWindow;
 	}
 	
 }
